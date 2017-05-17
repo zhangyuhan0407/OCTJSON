880b565a43a63a64531bb4363cdd416409389c94
yuhan <zhangyuhan0407@hotmail.com>
2017-05-17 12:29:52 +0800

    v1.6.8

--- Sources/OCTJSON.swift
+++ Sources/OCTJSON.swift
@@ -519,39 +519,39 @@ extension String {
 
 
 
-//public extension JSON {
-//    
-//    public static func read(fromFile file: String) -> JSON? {
-//        
-//        guard let data = FileManager.default.contents(atPath: file) else {
-//            return nil
-//        }
-//        
-//        guard let s = String(data: data, encoding: String.Encoding.utf8) else {
-//            return nil
-//        }
-//        
-//        do {
-//            let json = try JSON.deserialize(s)
-//            return json
-//        } catch {
-//            return nil
-//        }
-//        
-//    }
-//    
-//    
-//    public mutating func update(value: Any, forKey key: String) {
-//        self[key] = JSON(value)
-//    }
-//    
-//    public mutating func add(value: Int, forKey key: String) {
-//        let count = self[key].int ?? 0
-//        self[key] = JSON(count + value)
-//    }
-//    
-//    
-//}
+public extension JSON {
+    
+    public static func read(fromFile file: String) -> JSON? {
+        
+        guard let data = FileManager.default.contents(atPath: file) else {
+            return nil
+        }
+        
+        guard let s = String(data: data, encoding: String.Encoding.utf8) else {
+            return nil
+        }
+        
+        do {
+            let json = try JSON.deserialize(s)
+            return json
+        } catch {
+            return nil
+        }
+        
+    }
+    
+    
+    public mutating func update(value: Any, forKey key: String) {
+        self[key] = JSON(value)
+    }
+    
+    public mutating func add(value: Int, forKey key: String) {
+        let count = self[key].int ?? 0
+        self[key] = JSON(count + value)
+    }
+    
+    
+}
 
 
 
